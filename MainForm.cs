cb15af91ec04728207cbfab8163521b38c15a57f
nfsl <kir.nik.nikita@gmail.com>
2021-04-09 16:56:45 +0400

    Создание проекта

---
+++ MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Construct
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+
+			InitializeComponent();
+			
+		}
+	}
+}
